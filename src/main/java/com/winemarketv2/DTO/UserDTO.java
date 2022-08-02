1bf359de6005b3a1e6d0996d0915d857068fdb79
nghia <nghiaplps17855@fpt.edu.vn>
2022-08-02 12:12:46 +0700

    fix err log x2
    change spring security, spring boot version

--- src/main/java/com/winemarketv2/DTO/UserDTO.java
+++ src/main/java/com/winemarketv2/DTO/UserDTO.java
@@ -1,19 +1,19 @@
-package com.winemarketv2.DTO;
+// package com.winemarketv2.DTO;
 
-import com.winemarketv2.entity.User;
+// import com.winemarketv2.entity.User;
 
-public class UserDTO {
+// public class UserDTO {
     
-    private String username;
-    private String email;
-    private String image;
-    private boolean role;
+//     private String username;
+//     private String email;
+//     private String image;
+//     private boolean role;
 
-    public UserDTO(User user){
-        this.username = user.getUsername();
-        this.email = user.getEmail();
-        this.image = user.getImage();
-        this.role = user.isRole();
-    }
+//     public UserDTO(User user){
+//         this.username = user.getUsername();
+//         this.email = user.getEmail();
+//         this.image = user.getImage();
+//         this.role = user.isRole();
+//     }
 
-}
+// }
--- src/main/java/com/winemarketv2/service/UploadService.java
+++ src/main/java/com/winemarketv2/service/UploadService.java
@@ -2,13 +2,6 @@ package com.winemarketv2.service;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
